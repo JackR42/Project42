086b8a6be546b077bd956bb7a7d35db37092e073
JackR42 <j.roumimper@gmail.com>
2022-08-01 22:52:03 +0200

    Fix

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ resource "azurerm_mssql_server" "project42" {
  name                         = "sqlserver42x679e6e9"
  version                      = "12.0"
  resource_group_name          = azurerm_resource_group.project42.name
+ location                     = azurerm_resource_group.project42.location
  administrator_login          = data.azurerm_key_vault_secret.secret1.value
  administrator_login_password = data.azurerm_key_vault_secret.secret2.value
 }
