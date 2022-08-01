52dcbf8043475a20b0701cad1e53fd97245f6d37
JackR42 <j.roumimper@gmail.com>
2022-08-02 00:46:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "azurerm_mssql_database" "project42" {
   auto_pause_delay_in_minutes = 60
   min_capacity                = 0.5
 
-  storage_account_type        = "Geo Local Zone"
+  storage_account_type        = "Local"
 
 }
 
