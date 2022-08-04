26fac46f76729fdb87f06479c7c5e12bd551138f
JackR42 <j.roumimper@gmail.com>
2022-08-04 22:14:09 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,9 @@
 #variable "env" {
 #    description = "The name of the environment"
 #}
-variable "resource-group-name" {
-    description = "The name of the resource group"
-}
+#variable "resource-group-name" {
+#    description = "The name of the resource group"
+#}
 variable "location-name" {
   description = "The name of the Azure Region in which all resources should be created."
 }
