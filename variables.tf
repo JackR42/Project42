af6d4015122ddcb8f13accdd0974d70c628734b6
JackR42 <j.roumimper@gmail.com>
2022-08-04 16:13:04 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,32 @@
-variable "rg-name" {
+# Generic variables
+variable "env" {
+    description = "The name of the environment"
+}
+variable "resource-group-name" {
     description = "The name of the resource group"
 }
-
-variable "rg-location" {
+variable "location-name" {
   description = "The name of the Azure Region in which all resources should be created."
 }
 
-variable "storage-account-name" {
-  description = "The name of the storage account"
-}
-
-variable "web-index" {
-  description = "The index document of the website"
-}
-
-variable "web-content" {
-  description = "This is the source content for the static website"
-}
-
+# Database variables
 variable "database-instance-name" {
   description = "This is the name for the SQL Server Instance"
 }
-
+variable "database-instance-name-fqdn" {
+  description = "This is the FQDNname for the SQL Server Instance"
+}
 variable "database-name" {
   description = "This is the name for the database"
+}
+
+# Static website variables
+variable "storage-account-name" {
+  description = "The name of the storage account"
+}
+variable "web-index" {
+  description = "The index document of the website"
+}
+variable "web-content" {
+  description = "This is the source content for the static website"
 }
