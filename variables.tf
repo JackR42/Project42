0bb8ec5ad1b9c239e68387374a57a378f7ac12e3
JackR42 <j.roumimper@gmail.com>
2022-08-05 00:33:49 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,23 +13,29 @@ variable "location-name" {
 }
 
 # Database variables
-#variable "database-instance-name" {
-#  description = "This is the name for the SQL Server Instance"
-#}
-#variable "database-instance-name-fqn" {
-#  description = "This is the FQDNname for the SQL Server Instance"
-#}
-#variable "database-database1-name" {
-#  description = "This is the name for the database"
-#}
+variable "database-instance-name" {
+  description = "This is the name for the SQL Server Instance"
+  default = "database-instance-default"
+}
+variable "database-instance-name-fqn" {
+  description = "This is the FQDNname for the SQL Server Instance"
+  default = "database-instance-fqdn-default"
+}
+variable "database-database1-name" {
+  description = "This is the name for the database"
+  default = "database-database1-default"
+}
 
 # Static website variables
-#variable "web-storage-account-name" {
-#  description = "The name of the storage account"
-#}
-#variable "web-index-document" {
-#  description = "The index document of the website"
-#}
-#variable "web-source-content" {
-#  description = "This is the source content for the static website"
-#}
+variable "web-storage-account-name" {
+  description = "The name of the storage account"
+  default = "web-storage-account-default"
+}
+variable "web-index-document" {
+  description = "The index document of the website"
+  default = "index.html"
+}
+variable "web-source-content" {
+  description = "This is the source content for the static website"
+  default = "<h1>Hello Website42!</h1>"
+}
