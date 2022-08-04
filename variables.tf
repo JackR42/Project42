7b98a1e864c6a36ad09ffc0b5cf04b8156da1bbe
JackR42 <j.roumimper@gmail.com>
2022-08-05 00:21:37 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,9 +5,11 @@ variable "env" {
 }
 variable "resource-group-name" {
     description = "The name of the resource group"
+    default = "rg-default"
 }
 variable "location-name" {
   description = "The name of the Azure Region in which all resources should be created."
+  default = "location-default"
 }
 
 # Database variables
